14dd4bee3d8a2ffbce840322a1d6ca16cc5a09bc
Kasper Glud Melgaard <kasperglud13@gmail.com>
2018-10-31 09:51:46 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,3 +2,5 @@
 all = color[0] + color[1] + color[2] + color[3] + color[4] + color[5] + color[6]
 color = ["red","orange","yellow","green","blue","indigo","violet"]
 def display_rainbow(color = "all")
+  puts "#{color}"
+end
